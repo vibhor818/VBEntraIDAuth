6a8fd5e787143d795b084075014c5d050b7f14f9
vibhor818 <vibhor818@gmail.com>
2026-07-26 00:02:46 +0530

    Delete VBEntraIDAuth directory

--- VBEntraIDAuth/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace VBEntraIDAuth.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
